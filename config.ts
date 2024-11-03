167d8b1f86c777fbb0e124bc9515b3b92f5e63e6
TresIqus94 <140224101+TresIqus94@users.noreply.github.com>
2024-11-02 22:06:20 -0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -94,7 +94,7 @@ export const config: Config = {
       ipv4: '74.91.115.72',
       gamePort: 2302,
       steamQueryPort: 27016,
-      cftoolsApiId: 'd_vpl3wnavWWRrdpypKoPbcZJT2SvCM8DGlRnglZXUc=',
+      cftoolsApiId: 'G/rhp2x9PZk0kNjWFQbxZnrcLbNkrBEsHWyPi5BrWF8=',
     },
   ],
   cftools: {
